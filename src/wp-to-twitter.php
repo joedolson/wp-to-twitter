729f6f808ea8c2265e9fa7e211acba5c13144f53
Joe Dolson <design@joedolson.com>
2018-04-20 17:11:49 -0500

    Update version numbers

--- src/wp-to-twitter.php
+++ src/wp-to-twitter.php
@@ -17,7 +17,7 @@
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/license/gpl-2.0.txt
  * Domain Path: lang
- * Version:     3.3.2
+ * Version:     3.3.3
  */
 
 /*
@@ -56,7 +56,7 @@ require_once( plugin_dir_path( __FILE__ ) . 'wpt-widget.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'wpt-rate-limiting.php' );
 
 global $wpt_version;
-$wpt_version = '3.3.2';
+$wpt_version = '3.3.3';
 
 add_action( 'init', 'wpt_load_textdomain' );
 /**
