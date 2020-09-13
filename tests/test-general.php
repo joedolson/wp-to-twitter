3b1e8ae13bcd8b8814f3197f41219116ce025d9a
Joe Dolson <design@joedolson.com>
2020-09-13 13:04:17 -0500

    Start updating for actual tests

---
+++ tests/test-general.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class Tests_WP_to_Twitter_General
+ *
+ * @package WP to Twitter
+ */
+
+/**
+ * Sample test case.
+ */
+class Tests_WP_to_Twitter_General extends WP_UnitTestCase {
+	/**
+	 * Verify not in debug mode.
+	 */
+	public function test_wpt_not_in_debug_mode() {
+		// Verify that the constant WPT_DEBUG is false.
+		$this->assertFalse( WPT_DEBUG );
+	}
+}
