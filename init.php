5dcdb81dc3419d5e24e862c955af45b42b1f9f0f
Joe Dolson <design@joedolson.com>
2022-11-27 13:43:41 -0600

    Init stub for running locally

---
+++ init.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * WP to Twitter
+ *
+ * @package     WP to Twitter
+ * @author      Joe Dolson
+ * @copyright   2008-2022 Joe Dolson
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: WP to Twitter
+ * Plugin URI:  http://www.joedolson.com/wp-to-twitter/
+ * Description: Posts a Tweet when you update your WordPress blog or post a link, using your URL shortener. Many options to customize and promote your Tweets.
+ * Author:      Joseph C Dolson
+ * Author URI:  http://www.joedolson.com
+ * Text Domain: wp-to-twitter
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/license/gpl-2.0.txt
+ * Domain Path: lang
+ * Version:     3.6.2
+ */
+
+/*
+	Copyright 2011-2022  Joe Dolson (email : joe@joedolson.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+include( dirname( __FILE__ ) . '/src/wp-to-twitter.php' );
