7adf9f64583bbfb07583110d0129391e40e41f76
Joe Dolson <design@joedolson.com>
2024-01-28 15:09:20 -0600

    Copyright

--- init.php
+++ init.php
@@ -21,7 +21,7 @@
  */
 
 /*
-	Copyright 2011-2023  Joe Dolson (email : joe@joedolson.com)
+	Copyright 2011-2024  Joe Dolson (email : joe@joedolson.com)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
